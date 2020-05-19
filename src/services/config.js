d18a3c98cfcf32b452d2aaf69a4c794b4f7057af
Sean <sean.cowan1@gmail.com>
2020-05-19 16:54:30 -0400

    updated the config file to work with heroku

--- src/services/config.js
+++ src/services/config.js
@@ -1,5 +1,5 @@
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT: 'https://secure-inlet-58346.herokuapp.com/api',
     TOKEN_KEY: 'findthatmovie-auth-token',
     CLIENT_API_KEY: 'f36d54c6-47c9-43de-aa5a-835ae17bdaba',
     EXT_API_ENDPOINT: 'https://api.themoviedb.org/3',
